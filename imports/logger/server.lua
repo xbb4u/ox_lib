c33aedb62060b79eacdddd63b64d71da392291e2
xbb4u <118782227+xbb4u@users.noreply.github.com>
2024-12-04 08:53:11 +0000

    Update server.lua

--- imports/logger/server.lua
+++ imports/logger/server.lua
@@ -216,7 +216,7 @@ if service == 'loki' then
         return bTable -- Return the new table of kvps
     end
 
-    function lib.logger(source, event, message, ...)
+    function lib.logger(source, resource, event, message, ...)
         if not buffer then
             buffer = {}
 
@@ -245,7 +245,7 @@ if service == 'loki' then
         -- Initializes values table with the message
         local values = {message = message}
 
-        -- Format the args into strings
+        -- Format the args into strings.3022514
         local tags = formatTags(source, ... and string.strjoin(',', string.tostringall(...)) or nil)
         local tagsTable = convertDDTagsToKVP(tags)
 
@@ -258,7 +258,7 @@ if service == 'loki' then
         local payload = {
             stream = {
                 server = hostname,
-                resource = cache.resource,
+                resource = resource or cache.resource,
                 event = event
             },
             values = {
